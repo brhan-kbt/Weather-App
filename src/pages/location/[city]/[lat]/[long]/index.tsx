19865d3040d05cf55f79902e2a3f67e697e5d6a9
Berhanu Kebito <brhankbt63@gmail.com>
2023-11-03 10:14:15 +0300

    add loading

--- src/pages/location/[city]/[lat]/[long]/index.tsx
+++ src/pages/location/[city]/[lat]/[long]/index.tsx
@@ -10,6 +10,7 @@ import RainChart from "../../../../../../components/RainChart";
 import HumidityChart from "../../../../../../components/HumidityChart";
 import getBasePath from "../../../../../../lib/getBasePath";
 import cleanData from "../../../../../../lib/cleanData";
+import Loading from "./loading";
 
 export const revalidate = 60;
  function WeatherPage() {
@@ -49,20 +50,20 @@ export const revalidate = 60;
                 weatherData: dataToSend
               })
             });
-          
+
             if (!res.ok) {
               throw new Error(`Failed to fetch data. Status: ${res.status}`);
             }
-          
+
             const gptData = await res.json(); // Await the JSON conversion
-          
+
             const { content } = gptData;
             setGptContent(content);
           } catch (error) {
             console.error("Error fetching weather summary:", error);
             // Handle the error, set a default content, or show an error message
           }
-         
+
         } catch (error) {
           console.error("Error fetching data: ", error);
         }
@@ -72,7 +73,7 @@ export const revalidate = 60;
     fetchData();
   }, [router.query]);
 
- 
+
   // if(weatherData){
   //   const dataToSend = cleanData(weatherData ,city);
 
@@ -91,92 +92,98 @@ export const revalidate = 60;
   // }
 
   return (
-    <div className="flex flex-col min-h-screen md:flex-row">
 
-      {/* InformationalPanel */}
-      <InformationPanel city={city} long={long} lat={lat}
-      results={weatherData}
-      />
+    <>
+     {weatherData && <div className="flex flex-col min-h-screen md:flex-row">
 
+        {/* InformationalPanel */}
+        <InformationPanel city={city} long={long} lat={lat} results={weatherData} />
 
-      {weatherData && ( // Conditionally render when weatherData is not null
-        <div className="flex-1 p-5 lg:p-10">
-          <div className="p-5">
-            <div className="pb-5">
-              <h2 className="text-xl font-bold">Todays Overview</h2>
-              <p className="text-sm text-gray-400">
-                Last Updated at:{" "}
+        {weatherData && ( // Conditionally render when weatherData is not null
+          <div className="flex-1 p-5 lg:p-10">
+            <div className="p-5">
+              <div className="pb-5">
+                <h2 className="text-xl font-bold">Todays Overview</h2>
+                <p className="text-sm text-gray-400">
+                  Last Updated at:{" "}
 
-                {new Date(weatherData.current_weather.time).toLocaleString()}
-              </p>
-            </div>
+                  {new Date(weatherData.current_weather.time).toLocaleString()}
+                </p>
+              </div>
 
-            <div className="m-2 mb-10">
-              {/* Callout card */}
-             <CalloutCard
-              message={gptContent}/>
-            </div>
+              <div className="m-2 mb-10">
+                {/* Callout card */}
+              <CalloutCard
+                message={gptContent}/>
+              </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-5 m-2">
-              <StatCard
-               title="Maximum Temprature"
-               metric={`${weatherData.daily.temperature_2m_max[0].toFixed(1)} °`}
-               color="yellow"
-              />
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-5 m-2">
+                <StatCard
+                title="Maximum Temprature"
+                metric={`${weatherData.daily.temperature_2m_max[0].toFixed(1)} °`}
+                color="yellow"
+                />
 
-              <StatCard
-               title="Mnimum Temprature"
-               metric={`${weatherData.daily.temperature_2m_min[0].toFixed(1)} °`}
-               color="green"
-              />
+                <StatCard
+                title="Mnimum Temprature"
+                metric={`${weatherData.daily.temperature_2m_min[0].toFixed(1)} °`}
+                color="green"
+                />
 
-            <div>
-              <StatCard
-               title="UV index"
-               metric={`${weatherData.daily.uv_index_max[0].toFixed(1)} °`}
-               color="rose"
-               />
-               {
-                 Number(weatherData.daily.uv_index_max[0].toFixed(1))> 4 &&(
-                   <CalloutCard
-                   warning
-                   message={"The UV is high today, be sure to wear SPF!"}/>
+              <div>
+                <StatCard
+                title="UV index"
+                metric={`${weatherData.daily.uv_index_max[0].toFixed(1)} °`}
+                color="rose"
+                />
+                {
+                  Number(weatherData.daily.uv_index_max[0].toFixed(1))> 4 &&(
+                    <CalloutCard
+                    warning
+                    message={"The UV is high today, be sure to wear SPF!"}/>
 
-                 )
-               }
+                  )
+                }
 
-            </div>
-
-
-              <div className="flex space-x-3">
-              <StatCard
-               title="Wind Speed"
-               metric={`${weatherData.current_weather.windspeed} m/s`}
-               color="violet"
-              />
-
-              <StatCard
-               title="Wind Direction"
-               metric={`${weatherData.current_weather.winddirection} °`}
-               color="cyan"
-              />
               </div>
 
 
-            </div>
-            <hr className="my-5" />
+                <div className="flex space-x-3">
+                <StatCard
+                title="Wind Speed"
+                metric={`${weatherData.current_weather.windspeed} m/s`}
+                color="violet"
+                />
 
-            <div className="space-y-3">
-              <TempChart results={weatherData}/>
-              <RainChart results={weatherData}/>
-              <HumidityChart results={weatherData}/>
-            </div>
+                <StatCard
+                title="Wind Direction"
+                metric={`${weatherData.current_weather.winddirection} °`}
+                color="cyan"
+                />
+                </div>
 
+
+              </div>
+              <hr className="my-5" />
+
+              <div className="space-y-3">
+                <TempChart results={weatherData}/>
+                <RainChart results={weatherData}/>
+                <HumidityChart results={weatherData}/>
+              </div>
+
+            </div>
           </div>
-        </div>
-      )}
+        )}
 
-    </div>
+      </div>}
+
+      {
+        !weatherData &&
+        <Loading/>
+      }
+    </>
+
   );
 }
 
